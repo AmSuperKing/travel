dc514475c802e968cb633fd39983d0a49f1bfb94
AmSuperKing <wangjian5092@163.com>
2020-10-10 17:34:50 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
-
+// 使用fastClick自带的attach方法绑定到document.body上
+fastClick.attach(document.body)
 /* eslint-disable no-new */
+// ES6写法，键值对的键和值同名时直接一个即可
 new Vue({
   el: '#app',
   router,
