a0a1185516eda8fce51ca3af9f42f204c6f9c748
AmSuperKing <wangjian5092@163.com>
2020-10-15 18:41:43 +0800

    get city data by ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,49 +12,15 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">三亚</div>
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
-          <div class="item border-bottom">阿坝藏族羌族自治州</div>
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.key">{{innerItem.name}}</div>
         </div>
       </div>
     </div>
@@ -65,6 +31,10 @@
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
   }
