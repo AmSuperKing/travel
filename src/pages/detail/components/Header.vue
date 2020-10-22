bd4a6137721a1196f6a36e2666d4dd489cfba104
AmSuperKing <wangjian5092@163.com>
2020-10-22 11:14:35 +0800

    finish detail-header

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DeatilHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      // console.log(document.documentElement.scrollTop)
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  // 对activated定义的window全局事件解绑，不然其他可滚动页面也会被handleScroll影响
+  // 不对全局事件及时进行解绑，将可能会导致大量Bug
+  // 组件内部定义在元素上的事件作用在组件内部，不会对外部影响
+  // 但对全局对象进行的事件绑定就会影响
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .8rem
+    height .8rem
+    line-height .8rem
+    border-radius .4rem
+    text-align center
+    background rgba(0, 0, 0, 0.7)
+    .header-abs-back
+      color #fff
+      font-size .4rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-fixed-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+      color #ffffff
+</style>
